24afd930da6461ced5fb349cbc414d66ec89278f
Adlax <h.videlier@gmail.com>
2023-11-23 17:01:32 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CheckoutPage, ErrorPage, HomePage, PrivateRoutePage, ProductsPage, SingleProductPage, CartPage, AboutPage } from "./pages";
+import { Navbar, Sidebar, Footer } from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Navbar />
+			<Sidebar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/about" element={<AboutPage />} />
+				<Route path="/cart" element={<CartPage />} />
+				<Route path="/products" element={<ProductsPage />} />
+				<Route path="/products/:id" element={<SingleProductPage />} />
+				<Route path="/checkout" element={<CheckoutPage />} />
+				<Route path="/*" element={<ErrorPage />} />
+			</Routes>
+			<Footer />
+		</Router>
+	);
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { ProductsProvider } from "./store/context/products_context";
+import { FiltersProvider } from "./store/context/filters_context";
+import { CartProvider } from "./store/context/cart_context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<ProductsProvider>
+		<FiltersProvider>
+			<CartProvider>
+				<App />
+			</CartProvider>
+		</FiltersProvider>
+	</ProductsProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
